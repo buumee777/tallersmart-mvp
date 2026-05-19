53e9903feb24237a60d3410a05a34c4b17fc43b5
buumee777 <emlopez.lopez@hotmail.com>
2026-05-19 20:48:41 -0300

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-export const supabaseUrl = 'https://iqdcvznigerljqwwtlvs.supabase.co';
-export const supabaseAnonKey = 'sb_publishable_n08oiA1Cgg4ld42rjo4hPA_8upfmOmj';
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
